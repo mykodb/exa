ded8c78b365ea2a1627d820165355e301a6b8f51
mykodb <mykodb@qq.com>
2026-01-07 02:33:09 +0800

    修正

--- src/main/kotlin/cn/mykodb/exa/core/world/block/LavaSinkBlock.kt
+++ src/main/kotlin/cn/mykodb/exa/core/world/block/LavaSinkBlock.kt
@@ -8,32 +8,27 @@ import net.minecraft.world.level.block.EntityBlock
 import net.minecraft.world.level.block.entity.BlockEntity
 import net.minecraft.world.level.block.state.BlockState
 import net.minecraft.world.level.block.state.StateDefinition
-import net.minecraft.world.level.block.state.properties.BlockStateProperties
-import net.minecraft.world.level.block.state.properties.DirectionProperty
+import net.minecraft.world.level.block.state.properties.BlockStateProperties.HORIZONTAL_FACING
 import javax.annotation.Nullable
 
 
 class LavaSinkBlock(properties: Properties) : Block(properties.lightLevel { 15 }), EntityBlock {
 
-    companion object {
-        val FACING: DirectionProperty = BlockStateProperties.HORIZONTAL_FACING
-    }
-
     init {
         registerDefaultState(
             stateDefinition.any()
-                .setValue(FACING, Direction.NORTH)
+                .setValue(HORIZONTAL_FACING, Direction.NORTH)
         )
     }
 
     override fun createBlockStateDefinition(pBuilder: StateDefinition.Builder<Block, BlockState>) {
-        pBuilder.add(FACING)
+        pBuilder.add(HORIZONTAL_FACING)
     }
 
     @Nullable
     override fun getStateForPlacement(pContext: BlockPlaceContext): BlockState? {
         val facing = pContext.horizontalDirection
-        return defaultBlockState().setValue(FACING, facing.opposite)
+        return defaultBlockState().setValue(HORIZONTAL_FACING, facing.opposite)
     }
 
     override fun newBlockEntity(
